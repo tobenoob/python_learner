f21c5815f826df03b08280aa640bec7cb47651cb
tobenoob <yanbagusas@gmail.com>
2018-04-30 23:32:09 +0700

    This is New Files

---
+++ kalkulaotrinput.py
@@ -0,0 +1,9 @@
+#kalkulator input dengan python
+print('input nilai ke -1 ')
+a=int(input())
+
+print('input nilai ke -2 ')
+b=int(input())
+
+total = a + b
+print(int(total))
---
+++ tes.py
@@ -0,0 +1,7 @@
+#1st code for python
+
+print('What is your Name ?')
+#input is used for keyboard input
+name = input()
+print ('hy ' +name+ ' welcome')
+
